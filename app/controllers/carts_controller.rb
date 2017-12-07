917e3fbed0669e619867c9943fc8f9d8d5aa1de3
bingo8670 <307286130@qq.com>
2017-12-07 17:47:13 +0800

    add cart view helper

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,7 @@
 class CartsController < ApplicationController
   def add
-    cart = Cart.from_hash(session[:cart9487])
-    cart.add_item(params[:id])
-    session[:cart9487] = cart.serialize
+    current_cart.add_tiem(params[:id])
+    session[:cart9487] = current_cart.serialize
 
     redirect_to product_path, notice: "已加入购物车"
   end
